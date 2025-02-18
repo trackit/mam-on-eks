5f6bdd0585e9e745a364536268da4fb486d3b5b8
Leandro Mota <leandrosantosmota@gmail.com>
2025-02-18 18:29:14 -0300

    Updating provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+    }
+  }
+}
+
 provider "aws" {
   region  = var.aws_region
   profile = var.profile
@@ -7,6 +15,25 @@ provider "aws" {
       Environment = var.env
       Owner       = var.owner
       Project     = var.project
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.current.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }

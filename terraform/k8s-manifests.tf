60df123f9537746aa80625f1d9fe0d5e3493f340
Leandro Mota <leandrosantosmota@gmail.com>
2025-02-18 21:38:02 -0300

    Editing storage class management

--- terraform/k8s-manifests.tf
+++ terraform/k8s-manifests.tf
@@ -1,29 +1,14 @@
-data "kubectl_path_documents" "standard_sc" {
-  pattern = "../phraseanet/k8s-manifests/storageclass.yaml"
+data "template_file" "standard_sc" {
+  template = file("../phraseanet/k8s-manifests/storageclass.yaml.tpl")
+
+  vars = {
+    owner   = var.owner
+    project = var.project
+  }
 }
 
-# resource "kubectl_manifest" "standard_sc" {
-#   for_each         = toset(data.kubectl_path_documents.standard_sc.documents)
-#   yaml_body        = each.value
-#   apply_only       = true
-#   wait_for_rollout = false
-
-#   lifecycle {
-#     ignore_changes = [
-#       yaml_body
-#     ]
-#   }
-# }
-
 resource "kubectl_manifest" "standard_sc" {
-  for_each = toset([
-    templatefile("../phraseanet/k8s-manifests/storageclass.yaml", {
-      owner        = var.owner
-      project      = var.project
-    })
-  ])
-
-  yaml_body        = each.value
+  yaml_body        = data.template_file.standard_sc.rendered
   apply_only       = true
   wait_for_rollout = false
 
